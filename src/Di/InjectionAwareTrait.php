d2b2fa9d383e90a8b31dc674621fed667419e6dc
szytko <slawomir.zytko@gmail.com>
2015-11-06 11:40:02 +0100

    Added: Di injection trait

---
+++ src/Di/InjectionAwareTrait.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is part of Vegas package
+ *
+ * @author Slawomir Zytko <slawek@amsterdam-standard.pl>
+ * @company Amsterdam Standard Sp. z o.o.
+ * @homepage http://cmf.vegas
+ */
+
+namespace Vegas\Di;
+
+/**
+ * Trait InjectionAwareTrait
+ *
+ * Can be used for classes implementing InjectionAwareInterface
+ * Provides methods required by InjectionAwareInterface
+ *
+ * @package Vegas\Di
+ */
+trait InjectionAwareTrait
+{
+    /**
+     * Dependency injector
+     *
+     * @var \Phalcon\DiInterface $dependencyInjector
+     */
+    protected $di;
+
+    /**
+     * Sets the dependency injector
+     *
+     * @param \Phalcon\DiInterface $dependencyInjector
+     * @return $this
+     */
+    public function setDI(\Phalcon\DiInterface $dependencyInjector)
+    {
+        $this->di = $dependencyInjector;
+
+        return $this;
+    }
+
+    /**
+     * Returns the internal dependency injector
+     *
+     * @return \Phalcon\DiInterface
+     */
+    public function getDI()
+    {
+        return $this->di;
+    }
+}
